6cd30cb03b59d6eee02de50271e3e985c982812a
Adhii <adithyen1@gmail.com>
2024-08-21 12:27:15 +0530

    bug at circle

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Create from './Create'
 import axios from 'axios'
+import { BsCircleFill,BsFillCheckCircleFill,BsFillTrashFill } from "react-icons/bs";
 
 function Home() {
     const [todos,setTodos] = useState([])
@@ -9,6 +10,13 @@ function Home() {
         .then(result => setTodos(result.data))
         .catch(err => console.log(err))
     },[])
+
+    const handleEdit = (id)=>{
+        axios.put('http://localhost:3001/update/'+id)
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
   return (
     <div className="home">
         <h2>Todo List</h2>
@@ -19,8 +27,15 @@ function Home() {
             <div><h2>No Record</h2></div>
             :
             todos.map(todo => (
-                <div>
-                    {todo}
+                <div key={todo._id} className='task'>
+                    <div className='checkbox' onClick={() => handleEdit(todo._id)}>
+                        <BsCircleFill
+                         className='icon'/>
+                        <p>{todo.task}</p>
+                    </div>
+                    <div>
+                        <span><BsFillTrashFill className='icon'/></span>
+                    </div>
                 </div>
             ))
         }
